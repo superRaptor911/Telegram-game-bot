8917aa0f7147cf7aab5ab7145d469988c1365f7a
superRaptor911 <raptor.inc2018@gmail.com>
2022-12-01 23:21:03 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import TelegramBot from 'node-telegram-bot-api';
 import * as dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
